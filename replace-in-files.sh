dcf2c1ed7e8fdb75806f2ec54eb4ce6840fde3c3
bowerth <bo.werth@gmail.com>
2016-10-08 17:37:18 +0200

    edit logo

--- replace-in-files.sh
+++ replace-in-files.sh
@@ -4,23 +4,30 @@
 
 path="$HOME/Dropbox/Programming/R/Intro-to-R-Bootcamp/work"
 
-search_name_upper="images/"
-replace_name_upper="../images/"
-search_name_lower="uc_logo2.png"
-replace_name_lower="oecd_logo2.png"
+search_1="images\/"
+replace_1="..\/images\/"
+
+search_2="uc_logo2.png"
+replace_2="oecd_logo2.png"
+
+search_3="data\/"
+replace_3="..\/data\/"
+
 # search_date="2015-08-08"
 # replace_date="2015-12-16"
 # search_date="2015-12-16-"
 # replace_date=""
 
 cd $path
-FILES=$(find . -maxdepth 1 -type f)
+# FILES=test2.Rmd
+# FILES=$(find . -maxdepth 1 -type f)
+FILES=$(find *.Rmd -maxdepth 1 -type f)
 # echo $FILES
 
 for file in $FILES
 do
-    # sed 's/'$search_name_upper'/'$replace_name_upper'/g;s/'$search_name_lower'/'$replace_name_lower'/g' $path/${file} > $path/${file}.tmp
-    sed 's/'$search_name_upper'/'$replace_name_upper'/g;s/'$search_name_lower'/'$replace_name_lower'/g' $path/${file} > $path/${file}.tmp
+    # sed 's/'$search_1'/'$replace_1'/g;s/'$search_2'/'$replace_2'/g' $path/${file} > $path/${file}.tmp
+    sed 's/'$search_1'/'$replace_1'/g;s/'$search_2'/'$replace_2'/g;s/'$search_3'/'$replace_3'/g' $path/${file} > $path/${file}.tmp
     rm $path/${file}
     mv $path/${file}.tmp $path/${file}
     echo "replaced strings in file ${file}"
