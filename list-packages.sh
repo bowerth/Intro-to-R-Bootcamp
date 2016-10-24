e109cfecb8d101c764cdb81b5d5f8b2530d7d9a5
bowerth <bo.werth@gmail.com>
2016-10-24 11:45:12 +0200

    list case study packages

--- list-packages.sh
+++ list-packages.sh
@@ -4,4 +4,4 @@ FOLDER=files-teacher
 
 cd $FOLDER
 
-grep -i 'library' 2-get-data.R 5-tidy-data.R 7-quickplots.R 8-ggplot.R > ../R-packages.txt
+grep -i 'library' 2-get-data.R 5-tidy-data.R 7-quickplots.R 8-ggplot.R 9-case-study-1/*.R 10-case-study-2.R > ../R-packages.txt
